c32090e81208a1b7cb0629a597de4d0d3eae83f7
Andrew Eo <chiajun97@gmail.com>
2022-08-03 22:44:30 +0800

    added project

---
+++ 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -0,0 +1,177 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { Country } from 'src/app/common/country';
+import { State } from 'src/app/common/state';
+import { ShoppeeFormService } from 'src/app/services/shoppee-form.service';
+
+
+@Component({
+  selector: 'app-checkout',
+  templateUrl: './checkout.component.html',
+  styleUrls: ['./checkout.component.css']
+})
+export class CheckoutComponent implements OnInit {
+
+  checkoutFormGroup: FormGroup;
+
+  totalPrice: number = 0;
+  totalQuantity: number = 0;
+  
+  creditCardYears: number[] = [];
+  creditCardMonths: number[] = [];
+
+  shippingAddressStates: State[] = [];
+  billingAddressStates: State[] = [];
+    
+
+  countries: Country[] = [];
+  
+  constructor(private formBuilder: FormBuilder,
+              private shoppeeFormService: ShoppeeFormService) { }
+
+  ngOnInit(): void {
+    
+    this.checkoutFormGroup = this.formBuilder.group({
+      customer: this.formBuilder.group({
+        firstName: new FormControl('', [Validators.required, Validators.minLength(2)]),
+        lastName: new FormControl('', [Validators.required, Validators.minLength(2)]),
+        email: new FormControl('',[Validators.required, Validators.pattern('^[a-z0-9._%+-]+@+[a-z0-9.-]+\\.[a-z]{2,4}$')])
+      }),
+      shippingAddress: this.formBuilder.group({
+        street: [''],
+        city: [''],
+        state: [''],
+        country: [''],
+        zipCode: ['']
+      }),
+      billingAddress: this.formBuilder.group({
+        street: [''],
+        city: [''],
+        state: [''],
+        country: [''],
+        zipCode: ['']
+      }),
+      creditCard: this.formBuilder.group({
+        cardType: [''],
+        nameOnCard: [''],
+        cardNumber: [''],
+        securityCode: [''],
+        expirationMonth: [''],
+        expirationYear: ['']
+      })
+    });
+
+    // populate credit card months
+    const startMonth: number = new Date().getMonth() + 1;
+    console.log("startMonth: " + startMonth);
+
+    this.shoppeeFormService.getCreditCardMonths(startMonth).subscribe(
+      data => {
+        console.log("Retrieved credit card months: " + JSON.stringify(data));
+        this.creditCardMonths = data;
+      }
+    );
+
+    // populate credit card years
+    this.shoppeeFormService.getCreditCardYears().subscribe(
+      data => {
+        console.log("Retrieved credit card years: " + JSON.stringify(data));
+        this.creditCardYears = data;
+      }
+    );
+
+    // populate the countries
+    this.shoppeeFormService.getCountries().subscribe(
+      data => {
+        console.log("Retrieved countries: " + JSON.stringify(data));
+        this.countries = data;
+      }
+    );
+
+  }
+
+  get firstName() { return this.checkoutFormGroup.get('customer.firstName'); }
+
+  get lastName() { return this.checkoutFormGroup.get('customer.lastName'); }
+  get email() { return this.checkoutFormGroup.get('customer.email'); }
+
+
+
+  copyShippingAddressToBillingAddress(event) {
+
+    if (event.target.checked) {
+      this.checkoutFormGroup.controls.billingAddress
+            .setValue(this.checkoutFormGroup.controls.shippingAddress.value);
+
+      // sync states
+      this.billingAddressStates = this.shippingAddressStates;
+    }
+    else {
+      this.checkoutFormGroup.controls.billingAddress.reset();
+      this.billingAddressStates = [];
+    }
+    
+  }
+
+  onSubmit() {
+    console.log("Handling the submit button");
+    if (this.checkoutFormGroup.invalid) {
+      this.checkoutFormGroup.markAllAsTouched();
+    }
+    
+    console.log(this.checkoutFormGroup.get('customer').value);
+    console.log("The email address is " + this.checkoutFormGroup.get('customer').value.email);
+    console.log("The shipping address is " + this.checkoutFormGroup.get('shippingAddress').value.country.name);
+    console.log("The shipping address is " + this.checkoutFormGroup.get('shippingAddress').value.state.name);
+
+  }
+
+  handleMonthsAndYears() {
+
+    const creditCardFormGroup = this.checkoutFormGroup.get('creditCard');
+
+    const currentYear: number = new Date().getFullYear();
+    const selectedYear: number = Number(creditCardFormGroup.value.expirationYear);
+
+    // if the current year equals the selected year, then start with the current month
+
+    let startMonth: number;
+
+    if (currentYear === selectedYear) {
+      startMonth = new Date().getMonth() + 1;
+    }
+    else {
+      startMonth = 1;
+    }
+
+    this.shoppeeFormService.getCreditCardMonths(startMonth).subscribe(
+      data => {
+        console.log("Retrieved credit card months: " + JSON.stringify(data));
+        this.creditCardMonths = data;
+      }
+    );
+  }
+
+  getStates(formGroupName: string) {
+    const formGroup = this.checkoutFormGroup.get(formGroupName);
+    const countryCode = formGroup.value.country.code;
+    // Debug usage: country Name 
+    const countryName = formGroup.value.country.name;
+    console.log(`${formGroupName} country code: ${countryCode}`);
+    console.log(`${formGroupName} country name: ${countryName}`);
+
+    this.shoppeeFormService.getStates(countryCode).subscribe(
+      data => {
+        // Check the form group name, assign state depend on formGroup
+        if (formGroupName === 'shippingAddress') {
+          this.shippingAddressStates = data;
+        } else {
+          this.billingAddressStates = data;
+        }
+
+        // select the first state as default
+        formGroup.get('state').setValue(data[0]);
+      }
+    );
+  }
+}
---
+++ README.md
@@ -0,0 +1,40 @@
+# E Commerce Platform - Shoppee
+
+Backend part is added to `.gitignore` due to security.
+
+## Fuctions Implemented
+
+1. Search by keyword/category
+   Our platform could search by both key word and category, and will return a page cannot found if not found anything.
+2. Shopping cart
+   ![Checkout Page](/project_showcase/ShoppingCart.PNG)
+   We can increment/decrement the Quantity of our product by clicking +/-.
+   We can also remove the product from our shopping cart by clicking remove button.
+3. Pagination    
+   
+   ![Pagination](/project_showcase/Pagination.PNG)
+   We added a feature by angular to let customer select how much item they want for each page, and button with boarders (max page show will be 5).
+4. Checkout page
+
+- Added forms for shipping address, billing address, and credit cart information
+- Added "Same as shipping address function"
+- Added selections to country while state(region) is synced with country (09092021)
+- Implementing the validation page (091822021): All fields required.
+<!-- TODO -->
+
+5. Registration Page
+<!-- TODO -->
+
+<!-- ## Platform Structure
+### Backend
+- Java
+- Spring Boot
+- Hibernate
+### Frontend
+- Angular
+- TypeScript
+- HTML/CSS -->
+
+
+## Credits
+1. CSS from [luv2code](http://www.luv2code.com/)
